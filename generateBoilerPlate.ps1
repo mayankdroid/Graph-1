4dbe0a49898e05f6e9d0d72b17055e567103172b
Rohit Kalhans <rohit.kalhans@gmail.com>
2016-03-19 18:06:24 +0530

    Added support for weighted graph

--- generateBoilerPlate.ps1
+++ generateBoilerPlate.ps1
@@ -19,7 +19,7 @@ Get-ChildItem "src\graph" -Filter *.java | `
 Foreach-Object{
     $content = (Get-Content $_.FullName ) -replace '/\*([^*]|[\r\n]|(\*+([^*/]|[\r\n])))*\*+/' ,''
     $imports += $content| Select-String -pattern '^[ \t]*import .*;'
-    $fileContents += $content| `
+    $filteredContent = $content| `
          Where {$_ -notmatch '^[ \t]*//'`
            -and $_ -notmatch '^[ \t]*package.*;' `
            -and $_ -notmatch '^[ \t]*import .*;' `
@@ -27,14 +27,15 @@ Foreach-Object{
            -and $_ -notmatch '^[ \t]*\/[\*]*' `
            -and $_ -notmatch '^[ \t]*\*\/'`
            -and $_ -notmatch '^[ \t]*\*'}
+           $fileContents += $filteredContent -replace '\\0' , ''
 }
 
 Write-SolutionFile "package generated;"
 Write-SolutionFile $imports
 Write-SolutionFile  " public class Solution{"
-Write-SolutionFile '//<editor-fold desc="Generated Code" defaultstate="collapsed">'
+Write-SolutionFile '    //<editor-fold desc="Generated Code For Graph Infra using github.com/rkalhans/Graph Do not edit inside this fold" defaultstate="collapsed">'
 Write-SolutionFile $fileContents
-Write-SolutionFile "//</editor-fold>"
+Write-SolutionFile "    //</editor-fold>"
 Write-SolutionFile "    public static void main(String[] args){"
 Write-SolutionFile "        // write your code here"
 Write-SolutionFile "    }"
